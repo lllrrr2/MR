d64e79a304a61a4ae96d8f0e49050b7ca05aae7a
ht944 <413255735@qq.com>
2023-10-12 11:12:50 +0800

    更新商品评价，删除失效脚本，更新依赖

---
+++ jd_comment_run.py
@@ -0,0 +1,16 @@
+'''
+new Env('京东商品评价');
+cron: 6 6 6 6 6 python3 jd_comment_run.py
+export RabbitToken="token值"
+'''
+
+import asyncio
+
+from jd_comment import JDComment
+from utils.common import TaskClass
+
+if __name__ == '__main__':
+    task = TaskClass("task")
+    task.name = "comment"
+    task.init_config(JDComment)
+    asyncio.run(task.main("京东商品评价"))
